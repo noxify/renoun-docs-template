eef29d5d330dbd95acd8428239d6495f102781fd
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2024-09-19 12:27:52 +0200

    * upgrade renoun to 4.1.0 and add required changes * removed not needed lit stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import createLitWrapper from "@lit-labs/nextjs"
 import createMDXPlugin from "@next/mdx"
 import { rehypePlugins, remarkPlugins } from "@renoun/mdx-plugins"
 
@@ -10,30 +9,29 @@ const withMDX = createMDXPlugin({
   },
 })
 
-const withLit = createLitWrapper({ addDeclarativeShadowDomPolyfill: true })
-
-export default withLit(
-  withMDX({
-    output: "export",
-    reactStrictMode: true,
-    trailingSlash: true,
-    poweredByHeader: false,
-    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-    eslint: { ignoreDuringBuilds: true },
-    typescript: { ignoreBuildErrors: true },
-    webpack(config, { webpack }) {
-      config.plugins.push(
-        new webpack.ContextReplacementPlugin(
-          /\/(@ts-morph\/common)\//,
-          (data) => {
-            for (const dependency of data.dependencies) {
-              delete dependency.critical
-            }
-            return data
-          },
-        ),
-      )
-      return config
-    },
-  }),
-)
+export default withMDX({
+  output: "export",
+  reactStrictMode: true,
+  trailingSlash: true,
+  poweredByHeader: false,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  webpack(config, { webpack }) {
+    config.resolve.extensionAlias = {
+      ".js": [".ts", ".tsx", ".js"],
+    }
+    config.plugins.push(
+      new webpack.ContextReplacementPlugin(
+        /\/(@ts-morph\/common)\//,
+        (data) => {
+          for (const dependency of data.dependencies) {
+            delete dependency.critical
+          }
+          return data
+        },
+      ),
+    )
+    return config
+  },
+})
