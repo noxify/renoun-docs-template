b874c0c8a82656f510d02e3df7facd9cefbf51c1
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2024-10-10 10:58:32 +0200

    back to the roots * Revert migration to turborepo * Removed the graphql playground * Removed the playwright playground * Backup created => branch: turborepo

--- content/docs/graphql/metadata.ts
+++
@@ -1,9 +0,0 @@
-import type { CollectionSchema } from "@/collections"
-import { GraphqlCollection } from "@/collections"
-
-export const metadata: CollectionSchema["metadata"] = {
-  title: "GraphQL docs",
-  entrypoint: "/docs/graphql",
-  alias: "graphql",
-  collection: GraphqlCollection,
-}
