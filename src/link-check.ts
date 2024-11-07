d3e56e027d5dbe66510f91c927135383d109f37a
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2024-11-07 15:06:30 +0100

    update link checker

--- src/link-check.ts
+++ src/link-check.ts
@@ -1,7 +1,11 @@
 import { readFileSync } from "fs"
+import { toString } from "mdast-util-to-string"
 import { printErrors, scanURLs, validateFiles } from "next-validate-link"
+import remarkParse from "remark-parse"
+import { unified } from "unified"
 
 import { CollectionInfo } from "./collections"
+import { createSlug } from "./lib/utils"
 
 function removeFromArray<T>(array: T[], valueToRemove: T[]): T[] {
   return array.filter((value) => !valueToRemove.includes(value))
@@ -40,12 +44,48 @@ const getFiles = async () => {
 
     const slug = removeFromArray(collection.getPathSegments(), ["docs"])
     const url = collection.getPath()
+    const content = readFileSync(filePath).toString()
+
+    const file = unified()
+      .use(remarkParse)
+      // it seems that something is weird with the headings
+      // because the tree isn't updated with the id's
+      //.use({ plugins: remarkPlugins })
+      .parse(content)
+
+    const headinsFromTree = file.children.filter(
+      (node) => node.type === "heading",
+    )
+
+    // this is just a copy from
+    // https://github.com/souporserious/renoun/blob/main/packages/mdx/src/remark/add-headings.ts
+    const headingCounts = new Map()
+    const headings = headinsFromTree.map((node) => {
+      const text = node.children.map((child) => toString(child)).join("")
+      let id = createSlug(text)
+
+      if (headingCounts.has(id)) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const count = headingCounts.get(id) + 1
+        headingCounts.set(id, count)
+        id = `${id}-${count}`
+      } else {
+        headingCounts.set(id, 1)
+      }
+
+      return {
+        text,
+        id,
+        depth: node.depth,
+      }
+    })
 
     result.push({
       path: filePath,
       url,
       slug,
-      content: readFileSync(filePath).toString(),
+      content,
+      headings: headings.map((ele) => ele.id),
     })
   }
 
@@ -61,7 +101,7 @@ const scanned = await scanURLs({
     "docs/[...slug]": [
       ...files.map((collection) => ({
         value: collection.slug,
-        //hashes: [],
+        hashes: collection.headings,
       })),
       ...directories.map((collection) => ({ value: collection.slug })),
     ],
